870419a7c78b444c493d90441621d56967beef75
danilofumuso <danilofumuso@gmail.com>
2024-09-04 16:41:12 +0200

    benchmark final commit v2

--- JavaScript/benchmark.js
+++ JavaScript/benchmark.js
@@ -81,7 +81,173 @@ const easy = [
   },
 ];
 
-const totalSeconds = 60;
+const medium = [
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "Science: Computers",
+    question: "What is the name of the default theme that is installed with Windows XP?",
+    correct_answer: "Luna",
+    incorrect_answers: ["Neptune", "Whistler", "Bliss"],
+  },
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "Science: Computers",
+    question: "Generally, which component of a computer draws the most power?",
+    correct_answer: "Video Card",
+    incorrect_answers: ["Hard Drive", "Processor", "Power Supply"],
+  },
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "Science: Computers",
+    question: "How fast is USB 3.1 Gen 2 theoretically?",
+    correct_answer: "10 Gb/s",
+    incorrect_answers: ["5 Gb/s", "8 Gb/s", "1 Gb/s"],
+  },
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "Science: Computers",
+    question: "What is the number of keys on a standard Windows Keyboard?",
+    correct_answer: "104",
+    incorrect_answers: ["64", "94", "76"],
+  },
+  {
+    type: "boolean",
+    difficulty: "medium",
+    category: "Science: Computers",
+    question: "AMD created the first consumer 64-bit processor.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "Science: Computers",
+    question: "The name of technology company HP stands for what?",
+    correct_answer: "Hewlett-Packard",
+    incorrect_answers: ["Howard Packmann", "Husker-Pollosk", "Hellman-Pohl"],
+  },
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "Science: Computers",
+    question: "Nvidia&#039;s headquarters are based in which Silicon Valley city?",
+    correct_answer: "Santa Clara",
+    incorrect_answers: ["Palo Alto", "Cupertino", "Mountain View"],
+  },
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "Science: Computers",
+    question: "In the server hosting industry IaaS stands for...",
+    correct_answer: "Infrastructure as a Service",
+    incorrect_answers: ["Internet as a Service", "Internet and a Server", "Infrastructure as a Server"],
+  },
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "Science: Computers",
+    question: "Which of these is the name for the failed key escrow device introduced by the National Security Agency in 1993?",
+    correct_answer: "Clipper Chip",
+    incorrect_answers: ["Enigma Machine", "Skipjack", "Nautilus"],
+  },
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "Science: Computers",
+    question: "What does AD stand for in relation to Windows Operating Systems? ",
+    correct_answer: "Active Directory",
+    incorrect_answers: ["Alternative Drive", "Automated Database", "Active Department"],
+  },
+];
+
+const hard = [
+  {
+    type: "multiple",
+    difficulty: "hard",
+    category: "Science: Computers",
+    question: "Which kind of algorithm is Ron Rivest not famous for creating?",
+    correct_answer: "Secret sharing scheme",
+    incorrect_answers: ["Hashing algorithm", "Asymmetric encryption", "Stream cipher"],
+  },
+  {
+    type: "multiple",
+    difficulty: "hard",
+    category: "Science: Computers",
+    question: "What does the International System of Quantities refer 1024 bytes as?",
+    correct_answer: "Kibibyte",
+    incorrect_answers: ["Kylobyte", "Kilobyte", "Kelobyte"],
+  },
+  {
+    type: "multiple",
+    difficulty: "hard",
+    category: "Science: Computers",
+    question: "What port does HTTP run on?",
+    correct_answer: "80",
+    incorrect_answers: ["53", "443", "23"],
+  },
+  {
+    type: "multiple",
+    difficulty: "hard",
+    category: "Science: Computers",
+    question: "The Harvard architecture for micro-controllers added which additional bus?",
+    correct_answer: "Instruction",
+    incorrect_answers: ["Address", "Data", "Control"],
+  },
+  {
+    type: "boolean",
+    difficulty: "hard",
+    category: "Science: Computers",
+    question: "The T-Mobile Sidekick smartphone is a re-branded version of the Danger Hiptop.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    type: "multiple",
+    difficulty: "hard",
+    category: "Science: Computers",
+    question: "What is the name given to layer 4 of the Open Systems Interconnection (ISO) model?",
+    correct_answer: "Transport",
+    incorrect_answers: ["Session", "Data link", "Network"],
+  },
+  {
+    type: "multiple",
+    difficulty: "hard",
+    category: "Science: Computers",
+    question: "What is the name of the process that sends one qubit of information using two bits of classical information?",
+    correct_answer: "Quantum Teleportation",
+    incorrect_answers: ["Super Dense Coding", "Quantum Entanglement", "Quantum Programming"],
+  },
+  {
+    type: "multiple",
+    difficulty: "hard",
+    category: "Science: Computers",
+    question: "Who is the original author of the realtime physics engine called PhysX?",
+    correct_answer: "NovodeX",
+    incorrect_answers: ["Ageia", "Nvidia", "AMD"],
+  },
+  {
+    type: "multiple",
+    difficulty: "hard",
+    category: "Science: Computers",
+    question: "Who invented the &quot;Spanning Tree Protocol&quot;?",
+    correct_answer: "Radia Perlman",
+    incorrect_answers: ["Paul Vixie", "Vint Cerf", "Michael Roberts"],
+  },
+  {
+    type: "multiple",
+    difficulty: "hard",
+    category: "Science: Computers",
+    question: "America Online (AOL) started out as which of these online service providers?",
+    correct_answer: "Quantum Link",
+    incorrect_answers: ["CompuServe", "Prodigy", "GEnie"],
+  },
+];
+
+const totalSeconds = 60; // funzionamento del timer
 let remainingSeconds = totalSeconds;
 
 const secondsElement = document.getElementById("seconds");
@@ -143,12 +309,16 @@ const quiz = function (index) {
     buttonsDiv.appendChild(button);
 
     button.addEventListener("click", function () {
-      let timerInterval = setInterval(updateTimer, 1000);
+      const buttons = document.querySelectorAll("button"); //questo disabilita i bottoni dopo il primo click
+      buttons.forEach((button) => (button.disabled = true));
+
+      let timerInterval = setInterval(updateTimer, 1000); //restarta il timer ad ogni domanda
       clearInterval(timerInterval);
       remainingSeconds = totalSeconds;
       updateTimer();
 
       if (button.innerText === easy[index].correct_answer) {
+        //fanno andare avanti i counter delle risposte
         counterRight++;
         button.classList.add("green");
         button.classList.remove("buttonHover");
@@ -162,6 +332,7 @@ const quiz = function (index) {
       }
 
       setTimeout(() => {
+        //questo timeout crea un delay per permettere la visualizzazione del cambio colore del bottone
         if (questionIndex !== easy.length - 1) {
           questionIndex++;
           quiz(questionIndex);
